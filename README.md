67325adab2681046fdc3b4fc983dec42710d00d3
hyuckkim <monkeyhyuck@gmail.com>
2024-02-15 09:01:34 +0900

    build: init

---
+++ README.md
@@ -0,0 +1,7 @@
+## 상자
+사진을 쌓아 두고 싶어요
+
+### 기술 스택
+- SvelteKit
+- Pico CSS
+- firebase
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    const e = (name: string) => {
+        console.log(`hello, ${name}!`);
+    }
+</script>
+<main class="container" style="margin-top: 40px">
+    <h1>상자</h1>
+    <blockquote>
+        <p>보물, 막대기, 잡동사니. 당신은 어떠신가요?</p>
+        <button>시작해보죠.</button>
+        <button class="outline">로그인</button>
+    </blockquote>
+</main>
